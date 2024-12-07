3babf7b37e276c0e57fd9a10817b1f61917fd1e3
Charly <charlyperezpop@gmail.com>
2024-12-07 17:46:35 -0600

    Toolbar buttons

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,11 +1,47 @@
 'use client'
+import { useEditorStore } from '@/store/use-editor-store'
+import Image from '@tiptap/extension-image'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
+import Underline from '@tiptap/extension-underline'
+import FontFamily from '@tiptap/extension-font-family'
+import TextStyle from '@tiptap/extension-text-style'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import ImageResize from 'tiptap-extension-resize-image'
 
 export const Editor = () => {
+    const { setEditor } = useEditorStore()
     const editor = useEditor({
+        onCreate: ({ editor }) => {
+            setEditor(editor)
+        },
+        onDestroy() {
+            setEditor(null)
+        },
+        onUpdate({ editor }) {
+            setEditor(editor)
+        },
+        onSelectionUpdate({ editor }) {
+            setEditor(editor)
+        },
+        onTransaction({ editor }) {
+            setEditor(editor)
+        },
+        onFocus({ editor }) {
+            setEditor(editor)
+        },
+        onBlur({ editor }) {
+            setEditor(editor)
+        },
+        onContentError({ editor }) {
+            setEditor(editor)
+        },
+        immediatelyRender: false,
         editorProps: {
             attributes: {
                 style: "padding-left:56px; padding-right:56px;",
@@ -14,12 +50,37 @@ export const Editor = () => {
         },
         extensions: [
             StarterKit,
+            FontFamily,
+            TextStyle,
+            Image,
+            ImageResize,
             TaskItem.configure({
                 nested: true,
             }),
-            TaskList
+            TaskList,
+            Table.configure({
+                resizable: true,
+            }),
+            TableRow,
+            TableHeader,
+            TableCell,
+            Underline,
         ],
-        content: '<p>Hello World! 🌎️</p>',
+        content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>`,
     })
     return (
         <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible '>
