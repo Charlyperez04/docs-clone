31895670fde3137db9288089a7ee93f1ce8b1481
Charly <charlyperezpop@gmail.com>
2025-01-20 10:25:44 -0600

    Auth & db setup

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { SearchInput } from './search-input'
+import { UserButton } from '@clerk/nextjs'
 
 
 const Navbar = () => {
@@ -18,7 +19,7 @@ const Navbar = () => {
                 <h3 className='text-xl'>Docs</h3>
             </div>
             <SearchInput />
-            <div></div>
+            <UserButton />
         </nav>
     )
 }
