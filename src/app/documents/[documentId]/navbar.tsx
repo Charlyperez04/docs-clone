fec50e052d4576646abc8f7c5bf0d783e14cd3a0
Charly <charlyperezpop@gmail.com>
2025-01-25 14:16:20 -0600

    Collaboration and notifications

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -18,6 +18,8 @@ import Image from "next/image"
 import Link from "next/link"
 import { BsFilePdf } from "react-icons/bs"
 import { DocumentInput } from "./document-input"
+import { Avatars } from "./avatars"
+import { Inbox } from "./inbox"
 export const Navbar = () => {
     const { editor } = useEditorStore()
 
@@ -208,6 +210,8 @@ export const Navbar = () => {
                 </div>
             </div>
             <div className='flex gap-3 items-center pl-6'>
+                <Avatars/>
+                <Inbox/>
                 <OrganizationSwitcher
                     afterCreateOrganizationUrl={'/'}
                     afterLeaveOrganizationUrl={'/'}
--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -1,19 +1,80 @@
 "use client";
 
+import { FullScreenLoader } from "@/components/fullscren-loader";
 import {
     ClientSideSuspense,
     LiveblocksProvider,
     RoomProvider,
 } from "@liveblocks/react/suspense";
 import { useParams } from "next/navigation";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useMemo, useState } from "react";
+import { toast } from "sonner";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { getDocuments, getUsers } from "./actions";
+
+type User = {
+    id: string;
+    name: string;
+    avatar: string;
+}
 
 export function Room({ children }: { children: ReactNode }) {
     const params = useParams();
+
+    const [users, setUsers] = useState<User[]>([]);
+
+    const fetchUsers = useMemo(
+        () => async () => {
+            try {
+                const list = await getUsers()
+                setUsers(list)
+            } catch {
+                toast.error("Failed to fetch users")
+            }
+        },
+        [],
+    )
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
     return (
-        <LiveblocksProvider publicApiKey={"pk_dev_5JbN1Atah3k5jzrHh3WiVBZneQaNZlv8r3kH-oUTTCxPZQ6P2f0R3ovWr0tLcu9k"}>
+        <LiveblocksProvider
+            throttle={16}
+            authEndpoint={async () => {
+                const enpoint = "/api/liveblocks-auth"
+                const room = params.documentId as string
+
+                const response = await fetch(enpoint, {
+                    method: "POST",
+                    body: JSON.stringify({ room }),
+
+                })
+                return await response.json()
+            }}
+            resolveUsers={({ userIds }) => {
+                return userIds.map((userId) => users.find((user) => user.id === userId) ?? undefined)
+            }}
+            resolveMentionSuggestions={({ text }) => {
+                let filteredUsers = users;
+
+                if (text) {
+                    filteredUsers = users.filter((user =>
+                        user.name.toLowerCase().includes(text.toLowerCase())
+                    ))
+                }
+                return filteredUsers.map((user) => user.id)
+            }}
+            resolveRoomsInfo={async ({ roomIds }) => {
+                const documents = await getDocuments(roomIds as Id<"documents">[])
+                return documents.map((document) => ({
+                    id: document.id,
+                    name: document.name
+                }))
+            }}
+        >
             <RoomProvider id={params.documentId as string}>
-                <ClientSideSuspense fallback={<div>Loading…</div>}>
+                <ClientSideSuspense fallback={<FullScreenLoader label="Room loading..." />}>
                     {children}
                 </ClientSideSuspense>
             </RoomProvider>
